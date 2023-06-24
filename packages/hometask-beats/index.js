7f9cdf279576b744418dd033283969f4c6d228de
Danila Kulinkovich <danek200326@gmail.com>
2023-06-24 15:15:29 +0300

    feat(ESDU-00): add burger

--- packages/hometask-beats/index.js
+++ packages/hometask-beats/index.js
@@ -14,3 +14,11 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
     },
   });
+
+  let menuBtn = document.querySelector('.header__button--burger');
+let menu = document.querySelector('.app__menu');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
